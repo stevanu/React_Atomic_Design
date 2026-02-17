d292082379723438c131e7a6d7f053f3e6381657
stevanu <ivanstevanu@gmail.com>
2026-02-17 21:54:55 +0700

    add form register

---
+++ src/components/Fragments/FormRegister.jsx
@@ -0,0 +1,42 @@
+import Button from "../Elements/Button/Index";
+import InputForm from "../Elements/Input/Index";
+
+const FormRegister = () => {
+  return (
+    <form action="">
+      <InputForm
+        label="Full Name"
+        type="text"
+        placeholder="Insert your name here"
+        name="fullname"
+        required
+      />
+      <InputForm
+        label="Email"
+        type="Email"
+        placeholder="Example@gmail.com"
+        name="Email"
+        required
+      />
+      <InputForm
+        label="password"
+        type="password"
+        placeholder="Password"
+        name="password"
+        required
+      />
+      <InputForm
+        label="Confirm Password"
+        type="password"
+        placeholder="*******"
+        name="Confirm Password"
+        required
+      />
+      <Button className="bg-blue-600" type="submit">
+        Register
+      </Button>
+    </form>
+  );
+};
+
+export default FormRegister;
