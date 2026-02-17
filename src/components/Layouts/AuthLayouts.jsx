024da95c55198430fb850ccbb34116d947870f89
stevanu <ivanstevanu@gmail.com>
2026-02-18 02:16:38 +0700

    router

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,14 +1,16 @@
 const AuthLayout = (props) => {
   const { children, title } = props;
   return (
-    <div className="w-full max-w-xs rounded-md p-5">
-      <h1 className="text-3xl text-blue-600 font-bold mb-2 text-center">
-        {title}
-      </h1>
-      <p className="font-medium text-slate-500 mb-5 text-center">
-        Welcome Please enter your details
-      </p>
-      {children}
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-xs rounded-md p-5">
+        <h1 className="text-3xl text-blue-600 font-bold mb-2 text-center">
+          {title}
+        </h1>
+        <p className="font-medium text-slate-500 mb-5 text-center">
+          Welcome Please enter your details
+        </p>
+        {children}
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ErrorPage from "./pages/404.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "hello",
+        element: <div>Hello World</div>,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
