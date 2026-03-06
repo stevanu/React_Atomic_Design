507469bffc391a0622bd4f34fb69100e45013ff1
stevanu <ivanstevanu@gmail.com>
2026-03-06 22:37:47 +0700

    adjust rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,28 +1,47 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Laptop baru",
+    price: "Rp 14.000.000",
+    image: "/images/laptop-1.jpg",
+    description: `lorem200 Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Sapiente unde deleniti exercitationem! Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Sapiente unde deleniti exercitationem!`,
+  },
+  {
+    id: 1,
+    name: "Laptop baru",
+    price: "Rp 14.000.000",
+    image: "/images/laptop-1.jpg",
+    description: `lorem200 Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Sapiente unde deleniti exercitationem! Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Sapiente unde deleniti exercitationem!`,
+  },
+  {
+    id: 1,
+    name: "Laptop baru",
+    price: "Rp 14.000.000",
+    image: "/images/laptop-1.jpg",
+    description: `lorem200 Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Sapiente unde deleniti exercitationem! Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Sapiente unde deleniti exercitationem!`,
+  },
+];
+
 const ProductsPage = () => {
   return (
-    <div className="flex justify-center py-5 gap-5">
-      <div>
-        <CardProduct>
-          <CardProduct.Header image="/images/laptop-1.jpg" />
-          <CardProduct.Body title="Laptop">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-            unde deleniti exercitationem!
+    <div className="flex justify-center py-5">
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
           </CardProduct.Body>
-          <CardProduct.Footer price="Rp 14.000.000" />
+          <CardProduct.Footer price={product.price} />
         </CardProduct>
-      </div>
-      <div>
-        <CardProduct>
-          <CardProduct.Header image="/images/laptop-1.jpg" />
-          <CardProduct.Body title="Laptop">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-            unde deleniti exercitationem!
-          </CardProduct.Body>
-          <CardProduct.Footer price="Rp 14.000.000" />
-        </CardProduct>
-      </div>
+      ))}
     </div>
   );
 };
